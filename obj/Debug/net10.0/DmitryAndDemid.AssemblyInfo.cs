ef70c620d900b0c5015828a831cd5f0bbaa32423
iwuwka <iwuwka@korfex.com>
2026-05-06 22:26:44 +0700

    v2

--- obj/Debug/net10.0/DmitryAndDemid.AssemblyInfo.cs
+++ obj/Debug/net10.0/DmitryAndDemid.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DmitryAndDemid")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78fbc5edd5ef5f8a1ada02eb379cc77c55cd0102")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33a9e3d8ed65f783c9641e711362dbbc2bf29483")]
 [assembly: System.Reflection.AssemblyProductAttribute("DmitryAndDemid")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DmitryAndDemid")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
