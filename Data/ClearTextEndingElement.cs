c133fbba0b38aebbfacd84e6846d4b2329201021
iwuwka <iwuwka@korfex.com>
2026-07-22 19:19:15 +0700

    23.07.26

--- Data/ClearTextEndingElement.cs
+++ Data/ClearTextEndingElement.cs
@@ -7,7 +7,5 @@ namespace DmitryAndDemid.Data;
 public class ClearTextEndingElement : EndingElement
 {
     public override void Apply(EndingScreen screen)
-    {
-        screen.ClearText();
-    }
+        => screen.ClearText();
 }
