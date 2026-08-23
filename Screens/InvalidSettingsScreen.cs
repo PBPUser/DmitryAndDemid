26d713aad47ef18168e773a66f7cc1ba0bf534fe
iwuwka <iwuwka@DESKTOP-ECLHS2N>
2026-08-23 23:55:34 +0700

    23.08.26

---
+++ Screens/InvalidSettingsScreen.cs
@@ -0,0 +1,17 @@
+﻿using DmitryAndDemid.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DmitryAndDemid.Screens;
+
+internal class InvalidSettingsScreen : MenuScreen
+{
+    public InvalidSettingsScreen()
+    {
+        SetTitle(Runtime.CurrentRuntime.Textures["invalid_settings.png"]);
+        SetBackground(Runtime.CurrentRuntime.Textures["MenuBackground"]);
+
+        MenuItems.Add(new MenuItem(""))
+    }
+}
