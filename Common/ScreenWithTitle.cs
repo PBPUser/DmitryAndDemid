3f00e23eb3f79fc371cae91517ff89e7a9dca72f
iwuwka <iwuwka@DESKTOP-ECLHS2N>
2026-09-08 21:58:27 +0700

    8.09.26

--- Common/ScreenWithTitle.cs
+++ Common/ScreenWithTitle.cs
@@ -13,9 +13,9 @@ public abstract class ScreenWithTitle : Screen
         MenuTextureTarget = Scale(new(0, 0, 640, 135), Runtime.CurrentRuntime.Scale);
     }
     
-    private BasicTexture MenuTitleTexture;
     protected float TimeDisappearTitle = float.MaxValue;
     protected float TimeAppearTitle = float.MinValue;
+    private BasicTexture MenuTitleText09ure;
     private static Rect MenuTextureTarget;
     private static Rect MenuTextureSource;
     protected float AppearingTime = .5f;
