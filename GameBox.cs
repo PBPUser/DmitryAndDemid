7e130322f7bbf10b8e409b998e65b1aa93325fc1
iwuwka <iwuwka@DESKTOP-ECLHS2N>
2026-09-19 23:46:34 +0700

    19.09.26

--- GameBox.cs
+++ GameBox.cs
@@ -24,8 +24,8 @@ public class GameBox : IDisposable
     public RuntimeChapter? ChapterInfo;
     public RuntimeStageInfo? StageInfo;
     public Player Player;
-    public int Difficulty;
     public string ProtogonistId;
+    public int Difficulty;
     public int CurrentTick = 0;
     public int TickOffset = 0;
 
